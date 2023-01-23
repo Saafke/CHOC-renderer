2dc167d6d40d108a93e2907e8e4ff211f0f5f98c
Xavier Weber <xavierweber63@gmail.com>
2023-01-23 10:12:11 +0000

    Add bash script

--- run_folders_separately.sh
+++ run_folders_separately.sh
@@ -4,7 +4,7 @@ pwd
 
 conda activate /mnt/c7dd8318-a1d3-4622-a5fb-3fc2d8819579/CORSMAL/envs/choc-render-env
 
-for i in {1..5}
+for i in {23..140}
 do
 	echo "Starting $i"
 	/home/weber/Downloads/blender-3.3.0-linux-x64/blender \
